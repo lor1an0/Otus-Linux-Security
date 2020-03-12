4e00c960c342fc7d4c6d9e39bf683a57c20f9c75
lor1an0 <lordan111@yaex.ru>
2020-03-12 20:30:04 +0300

    Fixed homeword 05.02.20

--- Homework 05.02.20/task.sh
+++ Homework 05.02.20/task.sh
@@ -1,23 +1,14 @@
 # permit mount drives for user otus
-
 groupadd mount_group
 usermod -aG mount_group otus
-
-touch /etc/polkit-1/rules.d/10-mount.rules
-echo "polkit.addRule(function(action, subject) {
-    if ((action.id == "org.freedesktop.udisks2.filesystem-mount-system" ||
-             action.id == "org.freedesktop.udisks2.filesystem-mount") &&
-                     subject.isInGroup("mount_group")) {
-                             return polkit.Result.YES;
-                                 }
-                                 });" >> /etc/polkit-1/rules.d/10-mount.rules
-								 
-systemctl restart polkit.service
-								 
-# permit ssh login for user otus2 only from 12-00 to 20-00  every day
-echo "account    required     pam_time.so" >> /etc/pam.d/sshd
-echo "sshd;*;otus2;Al1200-2000" >> /etc/security/time.conf
-
+yum -y install udisks2
+yum -y install policycoreutils-python
+\cp --no-preserve=context /vagrant/files/10-mount.rules /etc/polkit-1/rules.d/10-mount.rules
+		
+#change allowed logon time for user otus2 from 12:00 to 20:00
+sed -ie '/account.*include.*password-auth/ a\account required pam_time.so' /etc/pam.d/sshd
+\cp -f --no-preserve=context /vagrant/files/time.conf /etc/security
+	
 # change root for user otus3 while login via ssh
 USR=otus3
 RD=/home/$USR/chroot
@@ -29,13 +20,9 @@ for dir in $DIRS; do
 mkdir -p $dir
 done
 
-CMDS="/bin/sh /bin/bash /bin/cat /bin/ls /bin/cp /bin/mv /bin/cd /bin/rm /bin/mkdir /usr/bin/id"
-LIBS=`ldd $CMDS|grep -v ':$'|grep -v "not a dynamic executable"|cut -f 3 -d " "|sort|uniq|sed 1d`
+CMDS="/usr/bin/whoami /bin/sh /bin/bash /bin/cat /bin/ls /bin/cp /bin/mv /bin/cd /bin/rm /bin/mkdir /usr/bin/id"
 
-for lib in $LIBS; do
-cp -f $lib .$lib
-done
-cp -f /lib64/ld-linux-x86-64.so.2 ./lib64
+\cp -Rf /usr/lib64 $RD
 
 for cmd in $CMDS; do
 cp -f $cmd .$cmd
@@ -45,17 +32,20 @@ mount --bind -nodev /dev $RD/dev
 mount --bind -nodev /dev/pts $RD/dev/pts
 mount --bind -nodev /dev/null $RD/dev/null
 
+chmod 777 /dev/null 
 chown root:root -R $RD
-chown $USR:$USR $RD/home/$USR
 chown root:root $RD/..
+chown $USR:$USR $RD/home/$USR
 chmod 755 $RD -R
 chmod 700 $RD/home/$USR
 
-echo "Match User $USR" >> /etc/ssh/sshd_config
-echo "	ChrootDirectory $RD" >> /etc/ssh/sshd_config
-
 cp /etc/profile $RD/etc
+cp /etc/passwd $RD/etc
+cp /etc/group $RD/etc
+cp /etc/nsswitch.conf $RD/etc
 echo "export PATH=$PATH:/bin" >> $RD/etc/profile
+  
+sudo \cp -f --no-preserve=context /vagrant/files/sshd_config /etc/ssh/sshd_config
 
 systemctl restart sshd
-
+systemctl restart polkit.service
